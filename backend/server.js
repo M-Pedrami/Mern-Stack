ea203e633704e8a2e760b977971d87b9437d59b9
Maziar Pedrami <awildroot@gmail.com>
2023-11-29 22:38:23 +0100

    setup BE routing and controller

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,9 @@ const express = require("express");
 const app = express();
 require("dotenv/config");
 const PORT = process.env.PORT || 8001;
+const router = require('./Routes/goalRoutes')
 
-app.get("/", (req, res) => {
-  res.send({ message: "You have hit the homepage" });
-});
+app.use('/goals', router);
 
 app.listen(PORT, () =>
   console.log(`server listening on http://localhost:${PORT}`)
